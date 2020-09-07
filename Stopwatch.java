e6428693adc5f57656ed1fad5ec329c12d0e02da
suwonBastion <67666965+suwonBastion@users.noreply.github.com>
2020-09-07 13:11:49 +0900

    Create Stopwatch.java

---
+++ Stopwatch.java
@@ -0,0 +1,56 @@
+package com.example.bastion.stopwatch;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import java.util.Locale;
+import android.os.Handler;
+import android.widget.TextView;
+
+public class StopwatchActivity extends Activity {
+
+    private  int seconds = 0;
+    private boolean running;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stopwatch);
+        runTimer();
+    }
+
+    public void onClickStart(View view) {
+        running = true;
+    }
+
+    public void onClickStop(View view) {
+        running = false;
+
+    }
+
+    public void onClickReset(View view) {
+        running = false;
+        seconds = 0;
+    }
+    private void runTimer() {
+        final TextView timeView = (TextView)findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds/3600;
+                int minutes = (seconds%3600)/60;
+                int secs = seconds%60;
+                String time = String.format(Locale.getDefault(),
+                        "%d:%02d:%02d", hours, minutes, secs);
+                timeView.setText(time);
+                if (running) {
+                    seconds++;
+                }
+                handler.postDelayed(this, 1000);
+            }
+        });
+    }
+
+
+}
